17bd46a021ad2b030692466f1978467c22a28422
yogeshwaran <yogeshwaran.a2006@gmail.com>
2024-04-24 13:35:46 +0530

    success

---
+++ mathser/mathser/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from mathapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('areaofrectangle/',views.rectarea,name="areaofrectangle"),
+    path('',views.rectarea,name="areaofrectangleroot")
+]
+
